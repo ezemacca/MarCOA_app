412d0109936891745071bfa33bd46ca7a91a8f8b
Ezequiel Miguel Maccarini <43655619+ezemacca@users.noreply.github.com>
2022-06-22 16:31:52 -0300

    funcionando en Heroku, pero no funcionan los estilos

--- resources/views/content/GestorProyectos.blade.php
+++ resources/views/content/GestorProyectos.blade.php
@@ -1,8 +1,6 @@
 
-<link href="/css/style.css" rel="stylesheet">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
@@ -12,8 +10,6 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 
 @extends('layouts.default')
-
-
 @section('content')
 <script>
 $(document).ready(function(){
