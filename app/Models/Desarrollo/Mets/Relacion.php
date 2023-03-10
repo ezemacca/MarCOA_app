9e299b3cda0577705b008972020ce37aa2643642
Ezequiel Miguel Maccarini <43655619+ezemacca@users.noreply.github.com>
2023-03-09 22:26:14 -0300

    fix

--- app/Models/Desarrollo/Mets/Relacion.php
+++ app/Models/Desarrollo/Mets/Relacion.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models\Desarrollo\Metadatos;
+namespace App\Models\Desarrollo\Mets;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
